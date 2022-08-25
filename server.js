d1f002b5d6143ce70322908380fc3621180daebb
Belcross17 <alfredochabes1@hotmail.com>
2022-08-25 17:10:26 -0400

    function created to make a new note

--- server.js
+++ server.js
@@ -5,6 +5,24 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+function makeNewNote(body, notesArray){
+    const newNote = body;
+    if(!Array.isArray(notesArray))
+        notesArray = [];
+    if(notesArray.length === 0)
+        notesArray.push(0);
+    
+    body.id = notesArray[0];
+    notesArray[0]++;
+
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote
+}
+
 app.listen(PORT, () => {
     console.log(`Server now on port ${PORT}.`);
 })
