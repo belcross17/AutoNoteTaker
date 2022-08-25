677d7aa6a884d7038591411cdd975599cefbdf2c
Belcross17 <alfredochabes1@hotmail.com>
2022-08-25 17:19:25 -0400

    created function to delete notes

--- server.js
+++ server.js
@@ -23,6 +23,32 @@ function makeNewNote(body, notesArray){
     return newNote
 }
 
+app.post('/api/notes', (req, res) => {
+    const newNote = makeNewNote(req.body, allNotes);
+    res.json(newNote);
+});
+
+function deleteNote(id, notesArray){
+    for (let i = 0; i < notesArray.length; i++) {
+        let note = notesArray[i];
+
+        if(note.id == id) {
+            notesArray.splice(i, 1)
+            fs.writeFileSync(
+                path.join(__dirname, './db/db.json'),
+                JSON.stringify(notesArray, null, 2)
+            );
+
+            break;
+        }
+    }
+}
+
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, allNotes);
+    res.json(true);
+})
+
 app.listen(PORT, () => {
     console.log(`Server now on port ${PORT}.`);
 })
