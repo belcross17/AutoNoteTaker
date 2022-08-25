f68d125dc3f136dfb91fe15e2f5fc941f560737d
Belcross17 <alfredochabes1@hotmail.com>
2022-08-25 17:39:03 -0400

    final changes to index and note html + added app.get to server.js

--- server.js
+++ server.js
@@ -5,7 +5,27 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const allNotes = require('./Develop/db/db.json')
+const allNotes = require('./Develop/db/db.json');
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+    res.json(allNotes.slice(1));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+})
 
 function makeNewNote(body, notesArray){
     const newNote = body;
